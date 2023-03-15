b3d1022f324fa7d8bd7d74718284f456e6a52d75
ohya06 <67494037+ohya06@users.noreply.github.com>
2023-03-15 17:59:58 -0400

    NavBar, But will change some design and buttons
    WIll change some buttons and design and animation later

--- app/src/main/java/edu/umich/aehill/reminiscetest/ScaffoldBack.kt
+++ app/src/main/java/edu/umich/aehill/reminiscetest/ScaffoldBack.kt
@@ -5,7 +5,10 @@ import android.content.Context
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -24,6 +27,31 @@ fun ScaffoldBack(context: Context, navController: NavHostController, customModif
                     fontSize = 20.sp
                 )
             })
+        },
+        bottomBar = {
+            BottomNavigation(
+                backgroundColor = MaterialTheme.colors.primary,
+                contentColor = Color.White
+            ) {
+                BottomNavigationItem(
+                    icon = { Icon(Icons.Filled.Home, "Home") },
+                    label = { Text("Home") },
+                    selected = false,
+                    onClick = { navController.navigate("home") }
+                )
+                BottomNavigationItem(
+                    icon = { Icon(Icons.Filled.AccountCircle, "Profile") },
+                    label = { Text("Profile") },
+                    selected = false,
+                    onClick = { navController.navigate("profile") }
+                )
+                BottomNavigationItem(
+                    icon = { Icon(Icons.Filled.Settings, "Settings") },
+                    label = { Text("Settings") },
+                    selected = false,
+                    onClick = { navController.navigate("settings") }
+                )
+            }
         },
         floatingActionButton = {
             FloatingActionButton(
