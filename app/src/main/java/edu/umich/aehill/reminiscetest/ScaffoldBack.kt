a3345631d328c6c21ce286cca35208ed67aa53b0
ohya06 <67494037+ohya06@users.noreply.github.com>
2023-03-16 11:28:39 -0400

    NavBar Airplane Icon

--- app/src/main/java/edu/umich/aehill/reminiscetest/ScaffoldBack.kt
+++ app/src/main/java/edu/umich/aehill/reminiscetest/ScaffoldBack.kt
@@ -1,5 +1,6 @@
 package edu.umich.aehill.reminiscetest.ui.theme
 
+import edu.umich.aehill.reminiscetest.R
 import android.annotation.SuppressLint
 import android.content.Context
 import androidx.compose.foundation.layout.*
@@ -9,9 +10,12 @@ import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.ui.res.painterResource
+import androidx.compose.foundation.Image
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
@@ -21,29 +25,61 @@ import androidx.navigation.NavHostController
 fun ScaffoldBack(context: Context, navController: NavHostController, customModifier: Modifier, navigateTo: String, content: @Composable() () -> Unit) {
     Scaffold(
         topBar = {
-            TopAppBar(title = {
-                Text(
-                    text = "Reminisce",
-                    fontSize = 20.sp
-                )
-            })
-        },
+            TopAppBar(
+                backgroundColor = Color.Transparent,
+                contentColor = Color.Transparent,
+                elevation = 0.dp,
+                title = {
+                    Text(
+                        text = "Reminisce",
+                        fontSize = 20.sp,
+                    )
+                },
+                actions = { // Add this block to include actions on the right side of the top app bar
+                    IconButton(
+                        onClick = {
+                            // Handle profile icon click
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.AccountCircle,
+                            contentDescription = "Profile",
+                            tint = MaterialTheme.colors.onBackground
+                        )
+                    }
+                }
+            )
+        }, // Set the Scaffold background color to transparent
         bottomBar = {
             BottomNavigation(
                 backgroundColor = MaterialTheme.colors.primary,
                 contentColor = Color.White
             ) {
                 BottomNavigationItem(
-                    icon = { Icon(Icons.Filled.Home, "Home") },
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Filled.Home,
+                            contentDescription = "Home",
+                            tint = Color.White // Apply the white color tint
+                        )
+                    },
                     label = { Text("Home") },
                     selected = false,
                     onClick = { navController.navigate("MainView") }
                 )
                 BottomNavigationItem(
-                    icon = { Icon(Icons.Filled.AccountCircle, "Profile") },
-                    label = { Text("Profile") },
+                    icon = {
+                        val airplaneIcon = painterResource(id = R.drawable.airplane_icon)
+                        Image(
+                            painter = airplaneIcon,
+                            contentDescription = "Current Trip",
+                            modifier = Modifier.size(22.dp), // Set the desired size for your icon
+                            colorFilter = ColorFilter.tint(Color.White)
+                        )
+                    },
+                    label = { Text("Current Trip") }, // Change the label to "Airplane"
                     selected = false,
-                    onClick = { navController.navigate("profile") }
+                    onClick = { navController.navigate("CompletedTripView") }
                 )
                 BottomNavigationItem(
                     icon = { Icon(Icons.Filled.Settings, "Settings") },
