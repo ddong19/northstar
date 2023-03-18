df40fcc9bb1f26c8146459e4424dc3e8faec76a4
ddong19 <dandong@umich.edu>
2023-03-18 19:28:43 -0400

    Figuring out how to mark current location

--- app/src/main/java/edu/umich/aehill/reminiscetest/TravelMapView.kt
+++ app/src/main/java/edu/umich/aehill/reminiscetest/TravelMapView.kt
@@ -2,6 +2,7 @@ package edu.umich.aehill.reminiscetest
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -10,6 +11,9 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import android.content.pm.PackageManager
+import androidx.compose.material.SnackbarDefaults.backgroundColor
+import androidx.core.content.ContextCompat.startActivity
 import androidx.navigation.NavHostController
 import edu.umich.aehill.reminiscetest.ui.theme.ScaffoldBack
 
@@ -19,6 +23,7 @@ import edu.umich.aehill.reminiscetest.ui.theme.ScaffoldBack
 fun TravelMapView(context: Context, navController: NavHostController, customModifier: Modifier) {
     ScaffoldBack(context = context, navController = navController, customModifier = customModifier, content = {
         Greeting("you are in the travel map page view")
+
         FloatingActionButton(
             backgroundColor = Color(0xFFFFC107),
             contentColor = Color(0xFF00FF00),
