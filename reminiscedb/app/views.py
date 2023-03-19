90804666991215524d6db44be65186efd72e8167
tshilgalis <56262446+tshilgalis@users.noreply.github.com>
2023-03-19 16:05:24 -0400

    postimage return id fix

--- reminiscedb/app/views.py
+++ reminiscedb/app/views.py
@@ -121,7 +121,7 @@ def postimage(request):
     data = (trip_id, image_locations, image_uri)
     cursor.execute(insert_stmt, data)
 
-    data = connection.insert_id()
+    data = cursor.lastrowid()
     response = {}
     response['image_id'] = data
 
