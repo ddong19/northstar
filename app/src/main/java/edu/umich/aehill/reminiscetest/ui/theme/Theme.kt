10491da419e5b0fb26ec27127ef713185ce408e3
ohya06 <67494037+ohya06@users.noreply.github.com>
2023-03-19 18:37:04 -0400

    Added to UI/Theme.kt fancylogin

--- app/src/main/java/edu/umich/aehill/reminiscetest/ui/theme/Theme.kt
+++ app/src/main/java/edu/umich/aehill/reminiscetest/ui/theme/Theme.kt
@@ -15,9 +15,13 @@ private val DarkColorPalette = darkColors(
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+//    primary = Purple500,
+//    primaryVariant = Purple700,
+//    secondary = Teal200
+    primary = LightBlue800,
+    primaryVariant = LightBlue500,
+    secondary = LightBlue900,
+    background = Color.DarkGray,
 
     /* Other default colors to override
     background = Color.White,
@@ -28,6 +32,20 @@ private val LightColorPalette = lightColors(
     onSurface = Color.Black,
     */
 )
+
+@Composable
+fun FancyLoginScreenTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+
+    MaterialTheme(
+        colors = LightColorPalette,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
 
 @Composable
 fun ReminisceTestTheme(
