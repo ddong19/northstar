1203470993f9fd89ef0d36ace9a7552d36f768d1
ketakig101 <ketakig@umich.edu>
2023-03-19 07:10:45 -0400

    updates tripview to have media store for the images. Can only pick one image right now tho

--- app/src/main/java/edu/umich/aehill/reminiscetest/TripPageView.kt
+++ app/src/main/java/edu/umich/aehill/reminiscetest/TripPageView.kt
@@ -2,50 +2,43 @@ package edu.umich.aehill.reminiscetest
 
 
 import android.annotation.SuppressLint
-import android.app.Activity
 import android.content.Context
-import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
 import android.net.Uri
 import android.os.Build
-import android.os.ext.SdkExtensions.getExtensionVersion
+import android.provider.MediaStore
 import android.util.Log
-import android.util.Size
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.activity.ComponentActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
-import androidx.navigation.NavHostController
-import edu.umich.aehill.reminiscetest.ui.theme.ScaffoldBack
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.NavHostController
 import coil.compose.AsyncImage
-import android.widget.Toast
-import androidx.compose.ui.layout.ContentScale
+import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
+import edu.umich.aehill.reminiscetest.ui.theme.ScaffoldBack
 
 
 @Composable
-fun TripPageContent(context: Context){
+fun TripPageContent(context: Context, navController: NavHostController){
     Row(horizontalArrangement = Arrangement.SpaceBetween, modifier=Modifier.fillMaxWidth(1f)) {
         FloatingActionButton(
             backgroundColor = Color(0xFF808080),
@@ -53,7 +46,7 @@ fun TripPageContent(context: Context){
             modifier = Modifier.padding(35.dp, 90.dp, 0.dp, 0.dp),
 
             onClick = {
-
+                //
             }
         ) {
             Icon(Icons.Default.AddCircle, "add")
@@ -61,65 +54,60 @@ fun TripPageContent(context: Context){
         Text(
             //need to get tripLocation from input from the user
             text = "Cancun",
-            modifier = Modifier.padding(8.dp, 75.dp, 25.dp, 0.dp).fillMaxWidth(1f),
+            modifier = Modifier
+                .padding(8.dp, 75.dp, 25.dp, 0.dp)
+                .fillMaxWidth(1f),
             fontSize = 55.sp,
             textAlign = TextAlign.Right
         )
     }
+    var imageUri by remember { mutableStateOf<Uri?>(null) }
     val context = LocalContext.current
-    var imageUri: Any? by remember { mutableStateOf(R.drawable.baseline_add_circle_outline_24) }
-    /*val photoPicker = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.PickVisualMedia()
-    ) {
-        if (it != null) {
-            Log.d("PhotoPicker", "Selected URI: $it")
-            imageUri = it
-        } else {
-            Log.d("PhotoPicker", "No media selected")
+    val bitmap = remember { mutableStateOf<Bitmap?>(null) }
+
+    val launcher =
+        rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()) { uri: Uri? ->
+            imageUri = uri
         }
-    }*/
-    val multiplePhotoPicker = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.PickMultipleVisualMedia(maxItems = 2)
-    ) {
-        if (it != null) {
-            Log.d("PhotoPicker", "Selected URI: $it")
-            imageUri = it
-        } else {
-            Log.d("PhotoPicker", "No media selected")
-        }
-    }
 
     Column(
         modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        AsyncImage(
-            modifier = Modifier
-                .size(250.dp)
-                .clickable {
-                    multiplePhotoPicker.launch(
-                        PickVisualMediaRequest(
-                            ActivityResultContracts.PickVisualMedia.ImageOnly
-                        )
-                    )
-                },
-            model = ImageRequest.Builder(LocalContext.current)
-                .data(imageUri)
-                .crossfade(enable = true)
-                .build(),
-            contentDescription = "Avatar Image",
-            contentScale = ContentScale.Crop,
-        )
-        Spacer(modifier = Modifier.height(24.dp))
-        Button(onClick = {
-            Toast.makeText(
-                context,
-                ActivityResultContracts.PickVisualMedia.isPhotoPickerAvailable().toString(),
-                Toast.LENGTH_LONG
-            ).show()
-        }) {
-            Text(text = "Availability")
+
+        imageUri?.let {
+            if (Build.VERSION.SDK_INT < 28) {
+                bitmap.value = MediaStore.Images
+                    .Media.getBitmap(context.contentResolver, it)
+            } else {
+                val source = ImageDecoder.createSource(context.contentResolver, it)
+                bitmap.value = ImageDecoder.decodeBitmap(source)
+            }
+
+            bitmap.value?.let { btm ->
+                Image(
+                    bitmap = btm.asImageBitmap(),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(400.dp)
+                        .padding(20.dp)
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Button(onClick = { launcher.launch("image/*") }) {
+            Text(text = "Pick Image")
+        }
+        Spacer(modifier = Modifier.width(12.dp))
+        Button(
+            onClick = {
+                navController.navigate("CompletedTripView")
+            }
+        ) {
+            Text(text = "Finish Trip")
         }
     }
 }
@@ -129,19 +117,7 @@ fun TripPageContent(context: Context){
 @Composable
 fun TripPageView(context: Context, navController: NavHostController, customModifier: Modifier) {
     ScaffoldBack(context = context, navController = navController, customModifier = customModifier,
-        content = { TripPageContent(context = context)
-            FloatingActionButton(
-                backgroundColor = Color(0xFFFFC107),
-                contentColor = Color(0xFF00FF00),
-                modifier = Modifier.padding(300.dp, 300.dp, 8.dp, 8.dp),
-
-                onClick = {
-                    navController.navigate("CompletedTripView")
-                }
-            ) {
-                Icon(Icons.Default.ArrowForward, "fwd")
-            }
-        })
+        content = { TripPageContent(context = context, navController = navController) })
 }
 
 
