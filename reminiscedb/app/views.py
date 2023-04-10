3e79b52f8e5e8e7f2c5c8c78362dc12136168471
tshilgalis <56262446+tshilgalis@users.noreply.github.com>
2023-04-10 15:56:21 -0400

    deleteimage stmt reformat fix

--- reminiscedb/app/views.py
+++ reminiscedb/app/views.py
@@ -200,7 +200,7 @@ def deleteimage(request):
 
     cursor = connection.cursor()
     insert_stmt = (
-    "DELETE FROM images WHERE image_uri = image_uri RETURNING *;"
+    "DELETE FROM images WHERE image_uri = {} RETURNING *;".format(image_uri)
     )
 
     cursor.execute(insert_stmt, image_uri)
